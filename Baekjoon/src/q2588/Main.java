9348559db6f403aa72fd3f0da67214aed5e18b31
Class01 <Class01@DESKTOP-ILL1MFM>
2023-05-11 18:20:09 +0900

    백준 2588번 문제

---
+++ Baekjoon/src/q2588/Main.java
@@ -0,0 +1,15 @@
+package q2588;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		sc.close();
+		
+		System.out.printf("%1d",n2 * n1);
+	}
+
+}
