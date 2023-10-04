761901fc53063950a82c1f49540a6c98ed88be41
pressit <clue4828@gmail.com>
2023-10-04 18:46:29 +0900

    백준 2741번 문제

--- Baekjoon/src/q2741/Main.java
+++ Baekjoon/src/q2741/Main.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		sc.close();
-		
-		for(int i=1;i<=num;i++) {
+
+		// 1.
+		for (int i = 1; i <= num; i++) {
 			System.out.println(i);
 		}
+		System.out.println();
+
+		// 2.
+		int j = 1;
+		while (j <= num) {
+			System.out.println(j++);
+		}
+		System.out.println();
+
+		// 3.
+		int k = 1;
+		do {
+			System.out.println(k++);
+		} while (k <= num);
 	}
 }
