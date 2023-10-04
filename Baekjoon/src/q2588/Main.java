c028897b5ac6b275941e32e0d8aec9875fce7ad8
pressit <clue4828@gmail.com>
2023-10-04 18:46:36 +0900

    백준 2588번 문제

--- Baekjoon/src/q2588/Main.java
+++ Baekjoon/src/q2588/Main.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int n1 = sc.nextInt();
 		int n2 = sc.nextInt();
 		sc.close();
-		
-		System.out.printf("%1d",n2 * n1);
-	}
 
+		int s1 = n2 / 100;
+		int s2 = n2 % 100 / 10;
+		int s3 = n2 % 10;
+
+		System.out.println(n1 * s3);
+		System.out.println(n1 * s2);
+		System.out.println(n1 * s1);
+		System.out.println(n1 * n2);
+	}
 }
